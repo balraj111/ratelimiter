aa7c4c38bc2f7d0109f6a3dca1103e4e7e49ee84
balraj111 <balraj.svnit@gmail.com>
2025-07-11 04:18:26 +0530

    rest implementation as api gateway

--- cmd/ratelimiterd/main.go
+++ cmd/ratelimiterd/main.go
@@ -2,8 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"ratelimiter/internal/limiter"
+	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("running main")
+	r := limiter.NewLimiterFactory(limiter.LimiterConfig{
+		Limit:    5,
+		Interval: 10 * time.Second,
+		Type:     limiter.FixedWindow,
+	})
+
+	router := gin.Default()
+
+	router.Use(func(c *gin.Context) {
+		key := c.ClientIP()
+		if !r.Allow(key) {
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"error": "too many request",
+			})
+			return
+		}
+		c.Next()
+	})
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 5 * time.Second,
+	}
+
+	log.Println("starting server at :8080")
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Server failed: %v", err)
+	}
+
 }
